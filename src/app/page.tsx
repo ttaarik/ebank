e4f904c637356dc2e79fafadb94f438540ca6e6b
Tarik Kadric <kadric.tarik@hotmail.com>
2025-01-06 02:02:43 +0100

    added a simple login page and the name of the bank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Req() {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          toast("Nora Maus");
+          toast("Fetch was successfull");
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
@@ -18,8 +18,15 @@ export default function Req() {
   }, []);
 
   return (
-      <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-          <div className="w-full max-w-sm md:max-w-3xl">
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+          <div className="flex w-full max-w-sm flex-col gap-6">
+              <a href="#" className="flex items-center gap-2 self-center font-medium">
+                  {/*<div*/}
+                  {/*    className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">*/}
+
+                  {/*</div>*/}
+                  pLatin
+              </a>
               <LoginForm/>
           </div>
       </div>
